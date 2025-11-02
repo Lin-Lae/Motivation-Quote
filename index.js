e3ec9b62166afa56679705d39ad8bb8f6038b325
Lin <lynnlae22@gmail.com>
2025-11-02 15:32:22 +0530

    modified index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import ejs from "ejs";
 
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
